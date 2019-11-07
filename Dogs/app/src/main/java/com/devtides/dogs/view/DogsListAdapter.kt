39de144e2e02edf28e83fa1ce414544226cd544b
Carlos Garcia <carlos.garcia@gigigo.com.mx>
2019-11-07 17:48:51 -0600

    Se carga el proyecto al repositorio

---
+++ Dogs/app/src/main/java/com/devtides/dogs/view/DogsListAdapter.kt
@@ -0,0 +1,45 @@
+package com.devtides.dogs.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.devtides.dogs.R
+import com.devtides.dogs.databinding.ItemDogBinding
+import com.devtides.dogs.model.DogBreed
+import com.devtides.dogs.util.getProgressDrawable
+import com.devtides.dogs.util.loadImage
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogsListAdapter(val dogsList: ArrayList<DogBreed>) : RecyclerView.Adapter<DogsListAdapter.DogViewHolder>() {
+
+    fun updateDogList(newDogsLList: List<DogBreed>) {
+        dogsList.clear()
+        dogsList.addAll(newDogsLList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        //val view = inflater.inflate(R.layout.item_dog, parent, false)
+        val view = DataBindingUtil.inflate<ItemDogBinding>(inflater, R.layout.item_dog, parent, false)
+        return DogViewHolder(view)
+    }
+
+    override fun getItemCount() = dogsList.size
+
+    override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
+        holder.view.dog = dogsList[position]
+//        holder.view.name.text = dogsList[position].dogBreed
+//        holder.view.lifespan.text = dogsList[position].lifeSpan
+//        holder.view.setOnClickListener{
+//            Navigation.findNavController(it).navigate(ListFragmentDirections.actionDetailFragment())
+//        }
+//        holder.view.imageView.loadImage(dogsList[position].imageUrl, getProgressDrawable(holder.view.imageView.context))
+    }
+
+
+    class DogViewHolder(var view: ItemDogBinding) : RecyclerView.ViewHolder(view.root)
+}
---
+++ Dogs/app/src/main/java/com/devtides/dogs/viewmodel/DetailViewModel.kt
@@ -0,0 +1,15 @@
+package com.devtides.dogs.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.devtides.dogs.model.DogBreed
+
+class DetailViewModel : ViewModel() {
+
+    val dogLiveData = MutableLiveData<DogBreed>()
+
+    fun fetch() {
+        val dog = DogBreed("1", "Corgi", "15 years", "breedGroup", "bredFor", "temperament", "")
+        dogLiveData.value = dog
+    }
+}

371e8ced83e690bad90ed4a277577bdef237603b
Carlos Garcia <carlos.garcia@gigigo.com.mx>
2019-11-20 15:07:27 -0600

    pruebas con proyecto KotlinTutorial

--- KotlinTutorial/src/main/kotlin/Main.kt
+++ KotlinTutorial/src/main/kotlin/Main.kt
@@ -1,18 +1,3 @@
-import com.b2c.kotlintutorial.Loot
-import com.b2c.kotlintutorial.LootType
-import com.b2c.kotlintutorial.Player
-import com.b2c.kotlintutorial.Weapon
-
 fun main(args: Array<String>) {
-    val tim = Player("Tim")
-
-    tim.weapon = Weapon("Spear", 14)
-
-    val redPotion = Loot("Red Potion", LootType.POTION, 7.5)
-    tim.inventory.add(redPotion)
-    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOR, 80.00)
-    tim.inventory.add(chestArmor)
-
-    tim.showInventory()
-    println(tim)
+    println("Hello World")
 }
--- KotlinTutorial/src/main/kotlin/Weapon.kt
+++
@@ -1,8 +0,0 @@
-package com.b2c.kotlintutorial
-
-class Weapon(val name: String, val damageInflicted: Int = 1) {
-
-    override fun toString(): String {
-        return "$name inflicts $damageInflicted points of damage"
-    }
-}

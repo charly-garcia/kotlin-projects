b0a1ac0bc33ccee23288ab025646c4b0391e1883
carlos_gsanchez <carlos.garcia@gigigo.com>
2023-02-16 11:28:15 -0600

    otras pruebas tutorial compose

--- JetpackCompose/03SampleCompose2/app/src/main/java/com/grupoimagen/samplecompose2/MainActivity.kt
+++ JetpackCompose/03SampleCompose2/app/src/main/java/com/grupoimagen/samplecompose2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
@@ -16,12 +17,21 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.grupoimagen.samplecompose2.ui.theme.SampleCompose2Theme
+import android.content.res.Configuration
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ImageMessageCard()
+            SampleCompose2Theme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    //ImageMessageCard()
+                    //MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(SampleData.conversationSample)
+                }
+            }
         }
     }
 }
@@ -31,22 +41,40 @@ data class Message(val author: String, val body: String)
 @Composable
 fun MessageCard(msg: Message) {
     Column {
-        Text(text = msg.author)
+        Text(text = msg.author,
+            color = MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2)
         // Add a vertical space between the author and message texts
         Spacer(modifier = Modifier.height(4.dp))
-        Text(text = msg.body)
+        Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Text(
+                text = msg.body,
+                modifier = Modifier.padding(all = 4.dp),
+                style = MaterialTheme.typography.body2
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Colleague", "Hey, take a look at Jetpack Compose")
-    )
+    SampleCompose2Theme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MessageCard(
+                msg = Message("Colleague", "Hey, take a look at Jetpack Compose")
+            )
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+//@Preview(showBackground = true)
 @Composable
 fun ImageMessageCard() {
     Row(modifier = Modifier.padding(all = 8.dp)) {
@@ -58,6 +86,7 @@ fun ImageMessageCard() {
                 .size(80.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
 
         // Add a horizontal space between the image and the column
@@ -68,5 +97,22 @@ fun ImageMessageCard() {
                 msg = Message("Colleague", "Hey, take a look at Jetpack Compose")
             )
         }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    SampleCompose2Theme {
+        Conversation(SampleData.conversationSample)
     }
 }
---
+++ JetpackCompose/04SampleCompose3/app/src/main/java/com/example/samplecompose3/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.samplecompose3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.samplecompose3.ui.theme.SampleCompose3Theme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SampleCompose3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    //MessageCard(Message("Android", "Jetpack Compose"))
+                    Conversation(messages = SampleData.conversationSample)
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SampleCompose3Theme {
+        // A surface container using the 'background' color from the theme
+        Surface {
+            //MessageCard(
+            //    msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+            //)
+            Conversation(messages = SampleData.conversationSample)
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}

b6ef9bbbc7a31849fc2ef70040c1101c5ab46e7b
carlos_gsanchez <carlos.garcia@gigigo.com>
2023-05-23 10:21:54 -0600

    Se agrega UdemyJetpackCompose

--- Android12Intensivo/02PrimerApp/app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ Android12Intensivo/02PrimerApp/app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.hellokotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import android.widget.Button
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
@@ -17,12 +19,21 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         tts = TextToSpeech(this, this)
 
         //var message = findViewById<TextView>(R.id.textView)
-        textView = findViewById(R.id.textView)
+        textView = findViewById(R.id.tvStatus)
+        findViewById<Button>(R.id.btnPlay).setOnClickListener { speak() }
+    }
+
+    private fun speak() {
+        var message : String = findViewById<TextView>(R.id.tvStatus).text.toString()
+
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
+
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            textView.text = "Listo!"
+            textView.text = "Hello Kotlin!"
+            tts!!.setLanguage(Locale.US)
         } else {
             textView.text = "No disponible :("
         }

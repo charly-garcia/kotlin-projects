648a50e5006364b195ad1b3ea8f44b0eacd06d69
Carlos Garcia <gsanchez.carlos@gmail.com>
2023-06-02 12:38:48 -0600

    Se agrega prueba de Compose

---
+++ UdemyJetpackCompose/Seccion16/MovieApp/app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    MovieAppTheme {
+        Scaffold(topBar =  {
+            TopAppBar(title = {Text("Movies")},
+                background
+            )
+        }) {
+
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        Text(text = "Hello")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp {
+        MainContent()
+    }
+}
